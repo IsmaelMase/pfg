a55dfe867ead440f0a48fc1dab9d0d4a9e03fcc1
IsmaelMase <ismaelmfb@gmail.com>
2018-06-14 13:36:00 +0200

    commit

--- src/main/java/com/ismaelmasegosa/salerev/SalerevApplication.java
+++ src/main/java/com/ismaelmasegosa/salerev/SalerevApplication.java
@@ -1,17 +1,10 @@
 package com.ismaelmasegosa.salerev;
 
-import javax.annotation.Resource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ismaelmasegosa.salerev.service.serviceImpl.UploadService;
-
 @SpringBootApplication
 public class SalerevApplication {
-
-	@Resource
-	UploadService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalerevApplication.class, args);
